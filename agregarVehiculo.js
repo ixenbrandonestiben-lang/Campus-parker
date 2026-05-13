894a6f8be71eef42a450efe8c139095deb9d3ee1
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-05-13 09:11:13 -0600

    feat: :sparkles: comienzo de funciones con javascript
    enlace de archivos js a html para poder darle las funciones

---
+++ agregarVehiculo.js
@@ -0,0 +1,3 @@
+let parqueados = []
+
+
---
+++ main.js
@@ -0,0 +1,49 @@
+let datoscliente =
+{
+    "nombre": "admin",
+    "correo": "admin123@gmail.com",
+    "contraseña": "admin123..."
+}
+
+
+
+localStorage.setItem("datos", JSON.stringify(datoscliente))
+
+function inicioSesion(correIngresado, contraseñaIngresada) {
+    const datosSecion = JSON.parse(localStorage.getItem("datos"));
+
+    const nombre = datosSecion.nombre;
+
+    const correo = datosSecion.correo;
+
+    const contraseña = datosSecion.contraseña;
+
+    if (correIngresado == correo && contraseñaIngresada == contraseña) {
+
+        alert("Bienvenido ", nombre)
+        window.location.href = "dashboard.html"
+    
+    } else {
+    
+        alert("Error al ingresear el correo: ", correo, " o esta mal su contraseña: ", contraseña)
+
+    }
+}
+
+const btninicio = document.getElementById("btn-inicio")
+
+btninicio.addEventListener("click", function(){
+
+    btninicio.textContent = "cargando...";
+
+    const correoIngresado = document.getElementById("email").value
+
+    const contraseñaIngresada = document.getElementById("password").value
+
+
+    setTimeout(() => {
+        inicioSesion(correoIngresado, contraseñaIngresada);
+    }, 2000)
+})
+
+
