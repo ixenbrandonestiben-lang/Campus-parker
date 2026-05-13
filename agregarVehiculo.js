1bb8a8e6150ad49e088b61baf6f84d86893f73c3
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-05-13 09:25:04 -0600

    feat: :construction: archivo js de servicios
    agregando el archivo de javascript a el apartado de servicios.html para poder darle su mantenimiento

--- agregarVehiculo.js
+++ agregarVehiculo.js
@@ -1,3 +1,15 @@
 let parqueados = []
 
+// const placaIngresada = document.getElementById("placa")
 
+const seleccionarOpcion = document.getElementById("tipo")
+
+const opt = document.createElement("option")
+opt.textContent = "Moto"
+seleccionarOpcion.appendChild(opt)
+
+opt.textContent = "Carro"
+seleccionarOpcion.appendChild(opt)
+
+opt.textContent = "Bus"
+seleccionarOpcion.appendChild(opt)
